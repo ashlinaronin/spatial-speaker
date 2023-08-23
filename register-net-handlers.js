db45d3882742236c331e7e7563827dd7ddf18761
ashlinaronin <ashlinaronin@gmail.com>
2023-08-22 22:51:53 -0700

    test out server-based sequencer

--- register-net-handlers.js
+++ register-net-handlers.js
@@ -1,6 +1,28 @@
 module.exports = (io, socket, syncServer) => {
-  socket.on("gong", () => {
-    console.log("received gong msg, emitting");
-    io.emit("gong", { serverTime: syncServer.getSyncTime() + 2 });
-  });
+    socket.on("gong", () => {
+      console.log("received gong msg, emitting");
+      io.emit("gong", { serverTime: syncServer.getSyncTime() + 5 });
+    });
+
+//   const tempo = 15;
+//   let current16thNote = 0;
+//   let futureTickTime = syncServer.getSyncTime();
+//   const secondsPerBeat = 60.0 / tempo;
+
+//   const futureTick = () => {
+//     futureTickTime += secondsPerBeat;
+//     current16thNote = (current16thNote + 1) % 16;
+//   };
+
+//   const scheduleNote = (beatDivisionNumber, time) => {
+//     console.log(`scheduling tick ${beatDivisionNumber} at`, time + 5);
+//     io.emit("tick", { beatDivisionNumber, serverTime: time + 5 });
+//   };
+
+//   const seqInterval = setInterval(() => {
+//     while (futureTickTime < syncServer.getSyncTime() + 0.1) {
+//       scheduleNote(current16thNote, futureTickTime);
+//       futureTick();
+//     }
+//   }, 200);
 };
